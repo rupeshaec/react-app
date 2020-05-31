bde4ca338439486183004d2800591ed52898c7c5
Rupesh <rupesh.kumar@digitas.com>
2020-05-31 18:03:30 +1000

    Passing argument to handeler methods, passed Product obj with 'id' and 'name'

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 1, // toggle this to 0 or 1 to see changes
+    count: 0, // toggle this to 0 or 1 to see changes
     tags: ["tag1", "tag2", "tag3"], // toggle this to [] or ["tag1", "tag2", "tag3"] to see changes
   };
 
@@ -16,7 +16,10 @@ class Counter extends Component {
         <span style={this.styles} className={this.setBadgeColor()}>
           {this.formatCount()}
         </span>
-        <button className="btn btn-warning" onClick={this.handleIncrement}>
+        <button
+          className="btn btn-warning"
+          onClick={() => this.handleIncrement({ id: 1, name: "Product A" })}
+        >
           Increment
         </button>
         {this.renderList()}
@@ -50,16 +53,23 @@ class Counter extends Component {
     }
   }
 
-  handleIncrement = () => {
-    // notice the call to this method in onClick is not a method call. E.g. handleIncrement as oppose to handleIncrement()
-    console.error("Add more clicked");
-    console.error(this); // this will throw error if we remove bind from constructor.
-    // why 'this' is not available in this method. Since we called a reference to this method not actually called it.
-    this.setState({ count: this.state.count + 1 });
+  // handleIncrement = () => {
+  //   // notice the call to this method in onClick is not a method call. E.g. handleIncrement as oppose to handleIncrement()
+  //   console.error("Add more clicked");
+  //   console.error(this); // this will throw error if we remove bind from constructor.
+  //   // why 'this' is not available in this method. Since we called a reference to this method not actually called it.
+  //   this.setState({ count: this.state.count + 1 });
 
-    let arr = this.state.tags;
-    arr.push("tag" + (arr.length + 1));
-    this.setState({ tags: arr });
+  //   let arr = this.state.tags;
+  //   arr.push("tag" + (arr.length + 1));
+  //   this.setState({ tags: arr });
+  // };
+
+  handleIncrement = (product) => {
+    console.error("Product passed is ", product.id, product.name);
+    this.setState({ count: this.state.count + 1 });
+    // () => this.handleIncrement({ id: 1, name: "Product A" })
+    // means function call [() =>]followed by passing product as argument [JSON obj]
   };
 }
 
