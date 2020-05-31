c234e551be508ea504a88ff33a0976dbe2274906
Rupesh <rupesh.kumar@digitas.com>
2020-05-31 16:03:19 +1000

    Introduced className and added elements span and button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,12 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <div>
+        <span className="badge badge-primary m-2">1</span>
+        <button className="btn btn-warning">Increment</button>
+      </div>
+    );
   }
 }
 
