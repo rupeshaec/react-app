b55118d89daa893cedb8c0ef5118cce9f24696f9
Rupesh <rupesh.kumar@digitas.com>
2020-05-31 16:13:12 +1000

    Embedding expression, setting span value based on counter value

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,16 +2,21 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    count: 1, // toggle this to 0 or 1 to see changes
   };
 
   render() {
     return (
       <div>
-        <span className="badge badge-primary m-2">{this.state.count}</span>
+        <span className="badge badge-primary m-2">{this.formatCount()}</span>
         <button className="btn btn-warning">Increment</button>
       </div>
     );
+  }
+
+  formatCount() {
+    const counter = this.state.count;
+    return counter === 0 ? "Missing elements" : counter;
   }
 }
 
