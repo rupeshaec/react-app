06fb95d64e07824cd2db23e849370bb6eb5b046c
Rupesh <rupesh.kumar@digitas.com>
2020-05-31 16:09:07 +1000

    Dynamic value for span using 'state' obj

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0,
+  };
+
   render() {
     return (
       <div>
-        <span className="badge badge-primary m-2">1</span>
+        <span className="badge badge-primary m-2">{this.state.count}</span>
         <button className="btn btn-warning">Increment</button>
       </div>
     );
